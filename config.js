45abe93781daceed65b35ee9e15215dd17e7521d
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-08-16 21:16:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // config.js
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwGN44643IFsjAydMOCTVtyMhT5A2UtyiqZzyvgL8N-HdZUVk_gRpBwuo5oDNeIgWHF/exec'; // Replace with your deployed Apps Script URL
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw_H2XbrYILtC2TL3vHCaIjUAENVqe-Cbeh2G2EnUUbP4JbsnQTQFjEXhq_2k1a3cp0QQ/exec';
