f881c943bd261b2e799e559cdb65aaf5717fa816
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-08-16 22:35:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // config.js
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzBIt9R-oXiZdaQ5CRXgt4O0F_KwuuNXd7n2UOAkK_N3JFsbn99SmKEFRRQ_YGsl6b5eQ/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzwxavd6DYbm-mjqroKoQIRNcEy9uPOHEjQZr4vWJ3fXwnUMbb31-qUP7X9eyWoza3GPA/exec';
