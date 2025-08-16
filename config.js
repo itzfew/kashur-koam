b57c814edaaac6b29471380ee367f0b5cde41814
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-08-16 20:06:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Set this to your deployed Apps Script Web App URL (ends with /exec)
-const GAS_URL = "https://script.google.com/macros/s/REPLACE_WITH_YOUR_DEPLOYMENT_ID/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbwRsHAusg29OKweCUUj-89ApK44V5Lg0vuhsXQAjsgqezdtvPzxgIw5D9jDKfdKYw8Apg/exec";
